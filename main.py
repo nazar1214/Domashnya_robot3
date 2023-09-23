7846ae3200a8c7b2d7fe81bed7d70fe94d77b6f7
nazar1214 <nazikzubrik@gmail.com>
2023-09-23 22:17:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import random
-
 def guess_the_number():
     men = 0
     mat = 1000
     randmoit = random.randint(men, mat)
     popitka = 0
-
-    print("Вітаємо у грі 'Вгадай число'! Спробуйте вгадати число від 0 до 1000.")
+    print("вам треба вгадти число від 1000 до 0")
 
     while True:
             chos = int(input("Ваша спроба: "))
             popitka += 1
-
             if chos < randmoit:
                 print("Ви вибрали число яке менше яке випало")
             elif chos > randmoit:
